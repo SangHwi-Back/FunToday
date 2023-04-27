c073ae90e1015849c2faaea951efc070d31d5b03
백상휘 <sanghwiback@gmail.com>
2023-04-27 13:55:35 +0900

    Initial Commit

---
+++ FunToday/FunTodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunTodayApp.swift
+//  FunToday
+//
+//  Created by 백상휘 on 2023/04/27.
+//
+
+import SwiftUI
+
+@main
+struct FunTodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
