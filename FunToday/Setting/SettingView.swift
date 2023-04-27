1d18fcd911499a5b2746bafe97bdb7ec76d40bdc
백상휘 <sanghwiback@gmail.com>
2023-04-27 22:41:41 +0900

    Add simple setting list

--- FunToday/Setting/SettingView.swift
+++ FunToday/Setting/SettingView.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct SettingView: View {
     var body: some View {
-        Text("Hello, Setting!")
+      GeometryReader { proxy in
+        NavigationView {
+          List
+          {
+            Section {
+              Text("Users")
+              SettingRow(text: "로그아웃")
+              SettingRow(text: "개인정보 보호 동의 내역")
+            }
+            
+            Section {
+              Text("Contents")
+              SettingRow(text: "리뷰")
+              SettingRow(text: "활동 프리셋")
+            }
+          }
+        }
+      }
     }
 }
 
@@ -17,4 +34,15 @@ struct SettingView_Previews: PreviewProvider {
     static var previews: some View {
         SettingView()
     }
+}
+
+struct SettingRow: View {
+  var text: String
+  var body: some View {
+    HStack {
+      Text(text)
+      Spacer()
+    }
+    .padding(.horizontal, 8)
+  }
 }
